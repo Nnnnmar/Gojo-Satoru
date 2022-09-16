44cffa1add93a79fb16fc4256b7c7c3b5c21cf03
Nnnnmar <113699180+Nnnnmar@users.noreply.github.com>
2022-09-16 19:11:50 +0100

    Update

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['96891103640']
-global.premium = ['96891103640']
-global.ownernomer = '96891103640'
+global.owner = ['212625660239']
+global.premium = ['212625660239']
+global.ownernomer = '212625660239'
 global.ownername = '♛ الامبراطور اياتو ♛'
 global.botname = '♛ غوجو بوت ♛'
 global.footer = ' غوجو بوت ♛'
